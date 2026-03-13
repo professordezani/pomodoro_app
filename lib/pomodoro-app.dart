e1b0656ab59fb511e963f553f3b414154ea25990
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-13 16:40:31 -0300

    add audio player with bell sound

--- lib/pomodoro-app.dart
+++ lib/pomodoro-app.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ enum Status { shortBreak, workHard }
 
 class _PomodoroAppState extends State<PomodoroApp> {
   // atributos:
-  int seconds = 60;
+  int seconds = 30;
   var backgroundColor = Colors.green[50];
   var lightColor = Color.fromRGBO(77, 218, 110, 0.15);
   var darkColor = Color.fromRGBO(77, 218, 110, 0.62);
@@ -20,27 +21,59 @@ class _PomodoroAppState extends State<PomodoroApp> {
   var icon = Icons.coffee_outlined;
   var title = "Short Break";
   var status = Status.shortBreak;
+  var paused = true;
+  var timerIcon = Icons.play_arrow;
 
   Timer? _timer;
+  int increaseCount = 0;
 
   @override
   void initState() {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _cancel();
+    super.dispose();
+  }
+
   void _start() {
-    _timer = Timer.periodic(Duration(seconds: 1), _decrease1Second);
+    if (paused) {
+      _timer = Timer.periodic(Duration(seconds: 1), _decrease1Second);
+      paused = false;
+      timerIcon = Icons.pause;
+    }
+  }
+
+  void _cancel() {
+    if (!paused) {
+      _timer?.cancel();
+      paused = true;
+      timerIcon = Icons.play_arrow;
+      setState(() {});
+    }
   }
 
   void _decrease1Second(Timer _) {
-    if (seconds > 0) {
+    if (seconds > 1) {
       setState(() => seconds -= 1);
     } else {
       _changeStatus();
     }
+
+    if (seconds == 5) {
+      setState(() => backgroundColor = Colors.orange[50]);
+    }
+  }
+
+  void _playSound() {
+    var player = AudioPlayer();
+    player.play(AssetSource('sounds/bell.mp3'));
   }
 
   void _changeStatus() {
+    _playSound();
     if (status == Status.shortBreak) {
       status = Status.workHard;
       seconds = 60;
@@ -62,15 +95,21 @@ class _PomodoroAppState extends State<PomodoroApp> {
       icon = Icons.coffee_outlined;
       title = "Short Break";
     }
+    increaseCount = 0;
     setState(() {});
   }
 
-  void add10Seconds() {
-    setState(() => seconds += 10);
+  void _increase10Seconds() {
+    if (increaseCount < 6) {
+      increaseCount++;
+      setState(() => seconds += 10);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    var txtMinutes = (seconds ~/ 60).toString().padLeft(2, '0');
+    var txtSeconds = (seconds % 60).toString().padLeft(2, '0');
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Material(
@@ -94,7 +133,7 @@ class _PomodoroAppState extends State<PomodoroApp> {
                 ),
               ),
               Text(
-                "${seconds.toString().padLeft(2, '0')}\n00",
+                "$txtMinutes\n$txtSeconds",
                 style: TextStyle(
                   fontSize: 256,
                   color: fontColor,
@@ -109,7 +148,7 @@ class _PomodoroAppState extends State<PomodoroApp> {
                 children: [
                   InkWell(
                     borderRadius: BorderRadius.circular(32),
-                    onTap: add10Seconds,
+                    onTap: _increase10Seconds,
                     child: Container(
                       width: 80,
                       height: 80,
@@ -122,7 +161,7 @@ class _PomodoroAppState extends State<PomodoroApp> {
                   ),
                   InkWell(
                     borderRadius: BorderRadius.circular(32),
-                    onTap: _start,
+                    onTap: paused ? _start : _cancel,
                     child: Container(
                       width: 102,
                       height: 96,
@@ -130,12 +169,12 @@ class _PomodoroAppState extends State<PomodoroApp> {
                         color: darkColor,
                         borderRadius: BorderRadius.circular(32),
                       ),
-                      child: Icon(Icons.play_arrow, size: 32),
+                      child: Icon(timerIcon, size: 32),
                     ),
                   ),
                   InkWell(
                     borderRadius: BorderRadius.circular(32),
-                    onTap: () {},
+                    onTap: _changeStatus,
                     child: Container(
                       width: 80,
                       height: 80,
